e16c550d4a583e14dada67c49cea175a2791251d
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-04-11 16:42:01 +0900

    Change all to top level.

--- known_bugs.NoMonomorphism_slim/NoMonomorphism_slim.hs
+++ known_bugs.NoMonomorphism_slim/NoMonomorphism_slim.hs
@@ -1,71 +1,96 @@
 -- copy from lib/jhc/Numeric.hs
-expt :: Integer -> Int -> Integer
-expt base n = base^n
+x :: Double
+x = 0.0001
+base :: Integer
+base = 10
 
-floatToDigits :: (RealFloat a) => Integer -> a -> ([Int], Int)
+expt :: Int -> Integer
+expt n = base^n
 
-floatToDigits _ 0 = ([], 0)
-floatToDigits base x =
-    let (f0, e0) = decodeFloat x
-        (minExp0, _) = floatRange x
-        p = floatDigits x
-        b = floatRadix x
-        minExp = minExp0 - p            -- the real minimum exponent
+f0 :: Integer
+e0 :: Int
+(f0, e0) = decodeFloat x
 
-        -- Haskell requires that f be adjusted so denormalized numbers
-        -- will have an impossibly low exponent.  Adjust for this.
-        f :: Integer
-        e :: Int
-        (f, e) = let n = minExp - e0
-                 in  if n > 0 then (f0 `div` (b^n), e0+n) else (f0, e0)
+minExp0 :: Int
+(minExp0, _) = floatRange x
 
-        (r, s, mUp, mDn) =
-           if e >= 0 then
-               let be = b^e in
-               if f == b^(p-1) then
-                   (f*be*b*2, 2*b, be*b, b)
-               else
-                   (f*be*2, 2, be, be)
-           else
-               if e > minExp && f == b^(p-1) then
-                   (f*b*2, b^(-e+1)*2, b, 1)
-               else
-                   (f*2, b^(-e)*2, 1, 1)
-        k =
-            let k0 =
-                    if b==2 && base==10 then
-                        -- logBase 10 2 is slightly bigger than 3/10 so
-                        -- the following will err on the low side.  Ignoring
-                        -- the fraction will make it err even more.
-                        -- Haskell promises that p-1 <= logBase b f < p.
-                        (p - 1 + e0) * 3 `div` 10
-                    else
-                        ceiling ((log ((fromInteger (f+1))::Double) +
-                                 fromIntegral e * log (fromInteger b)) /
-                                  log (fromInteger base))
-                fixup n =
-                    if n >= 0 then
-                        if r + mUp <= expt base n * s then n else fixup (n+1)
-                    else
-                        if expt base (-n) * (r + mUp) <= s then n
-                                                           else fixup (n+1)
-            in  fixup (k0::Int)
+p :: Int
+p = floatDigits x
 
-        gen ds rn sN mUpN mDnN =
-            let (dn, rn') = (rn * base) `divMod` sN
-                mUpN' = mUpN * base
-                mDnN' = mDnN * base
-            in  case (rn' < mDnN', rn' + mUpN' > sN) of
-                (True,  False) -> dn : ds
-                (False, True)  -> dn+1 : ds
-                (True,  True)  -> if rn' * 2 < sN then dn : ds else dn+1 : ds
-                (False, False) -> gen (dn:ds) rn' sN mUpN' mDnN'
-        rds =
-            if k >= 0 then
-                gen [] r (s * expt base k) mUp mDn
+b :: Integer
+b = floatRadix x
+
+minExp :: Int
+minExp = minExp0 - p            -- the real minimum exponent
+
+-- Haskell requires that f be adjusted so denormalized numbers
+-- will have an impossibly low exponent.  Adjust for this.
+f :: Integer
+e :: Int
+(f, e) = let n = minExp - e0
+         in  if n > 0 then (f0 `div` (b^n), e0+n) else (f0, e0)
+
+mDn :: Integer
+mUp :: Integer
+s :: Integer
+r :: Integer
+(r, s, mUp, mDn) =
+   if e >= 0 then
+       let be = b^e in
+       if f == b^(p-1) then
+           (f*be*b*2, 2*b, be*b, b)
+       else
+           (f*be*2, 2, be, be)
+   else
+       if e > minExp && f == b^(p-1) then
+           (f*b*2, b^(-e+1)*2, b, 1)
+       else
+           (f*2, b^(-e)*2, 1, 1)
+k :: Int
+k =
+    let k0 =
+            if b==2 && base==10 then
+                -- logBase 10 2 is slightly bigger than 3/10 so
+                -- the following will err on the low side.  Ignoring
+                -- the fraction will make it err even more.
+                -- Haskell promises that p-1 <= logBase b f < p.
+                (p - 1 + e0) * 3 `div` 10
             else
-                let bk = expt base (-k)
-                in  gen [] (r * bk) s (mUp * bk) (mDn * bk)
-    in  (map fromIntegral (reverse rds), k)
+                ceiling ((log ((fromInteger (f+1))::Double) +
+                         fromIntegral e * log (fromInteger b)) /
+                          log (fromInteger base))
+        fixup n =
+            if n >= 0 then
+                if r + mUp <= expt n * s then n else fixup (n+1)
+            else
+                if expt (-n) * (r + mUp) <= s then n
+                                                   else fixup (n+1)
+    in  fixup (k0::Int)
 
-main = seq ((fst $ floatToDigits 10 0.0001) !! 0) $ return ()
+gen :: [Integer] -> Integer -> Integer -> Integer -> Integer -> [Integer]
+gen ds rn sN mUpN mDnN =
+    let (dn, rn') = (rn * base) `divMod` sN
+        mUpN' = mUpN * base
+        mDnN' = mDnN * base
+    in  case (rn' < mDnN', rn' + mUpN' > sN) of
+        (True,  False) -> dn : ds
+        (False, True)  -> dn+1 : ds
+        (True,  True)  -> if rn' * 2 < sN then dn : ds else dn+1 : ds
+        (False, False) -> gen (dn:ds) rn' sN mUpN' mDnN'
+rds :: [Integer]
+rds =
+    if k >= 0 then
+        gen [] r (s * expt k) mUp mDn
+    else
+        let bk = expt (-k)
+        in  gen [] (r * bk) s (mUp * bk) (mDn * bk)
+
+
+floatToDigits ::  ([Int], Int)
+floatToDigits = (map fromIntegral (reverse rds), k)
+
+main :: IO ()
+main = do
+  print k             -- => -3
+  print $ reverse rds -- => [1] will crash on ajhc
+  print floatToDigits -- => ([1],-3)
