ad9eec422ac7108ee0dcd834609301e12349b51a
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-06-05 19:50:01 +0900

    Add mutex interfaces.

--- try_pthread2/jhc-conc-pthread/include/OSThreads.h
+++ try_pthread2/jhc-conc-pthread/include/OSThreads.h
@@ -1,3 +1,14 @@
+#ifndef _OS_THREADS_H
+#define _OS_THREADS_H
+
 #include <pthread.h>
 
+typedef pthread_mutex_t jhc_mutex_t;
+
+#define jhc_mutex_init(M)	(void) pthread_mutex_init((M), NULL)
+#define jhc_mutex_lock(M)	pthread_mutex_lock((M))
+#define jhc_mutex_unlock(M)	pthread_mutex_unlock((M))
+
 pthread_t forkOS_createThread(void *entry, int *err);
+
+#endif /* _OS_THREADS_H */
