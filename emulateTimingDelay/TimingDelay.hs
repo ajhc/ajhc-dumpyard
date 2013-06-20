50b3bf742a1da72e00aad8a589fc5b9d32a60d3f
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-06-20 11:30:07 +0900

    Runnable demo.

--- emulateTimingDelay/TimingDelay.hs
+++ emulateTimingDelay/TimingDelay.hs
@@ -1,2 +1,34 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+import Data.Word
+import Control.Monad
+import Foreign.Ptr
+import Foreign.Storable
+import Jhc.Prim.Rts
+
+-- Timing
+foreign import ccall "c_extern.h getTimingDelay" c_gettimingDelay :: IO (Ptr Word32)
+
+timingDelayDecrement :: IO ()
+timingDelayDecrement = do
+  p <- c_gettimingDelay
+  i <- peek p
+  when (i >= 0) $ poke p (i - 1)
+
+foreign export ccall "timingDelayDecrement" timingDelayDecrement :: IO ()
+
+-- Delay
+myDelay :: Word32 -> IO ()
+myDelay nTime = do
+  p <- c_gettimingDelay
+  poke p nTime
+  let while :: IO ()
+      while = do
+        p' <- c_gettimingDelay
+        i <- peek p'
+        if (i > 0) then while else return ()
+  while
+
 main :: IO ()
-main = print "hoge"
+main = forever $ do
+  myDelay 5
+  putStrLn "Return myDelay."
--- emulateTimingDelay/main.c
+++ emulateTimingDelay/main.c
@@ -1,13 +1,40 @@
+#include <unistd.h>
 #include "jhc_rts_header.h"
+#include "rts/conc.h"
+#include "c_extern.h"
+
+extern void timingDelayDecrement(void);
+
+static uint32_t TimingDelay = 0;
+
+uint32_t *
+getTimingDelay()
+{
+        return &TimingDelay;
+}
+
+void *run_timingDelayDecrement(void *p)
+{
+	for (;;) {
+		sleep(1);
+		timingDelayDecrement();
+	}
+	/* NOTREACHED */
+	return NULL;
+}
 
 int
 main(int argc, char *argv[])
 {
+	int err;
+
         hs_init(&argc,&argv);
-        if (jhc_setjmp(&jhc_uncaught))
+        if (jhc_setjmp(&jhc_uncaught)) {
                 jhc_error("Uncaught Exception");
-        else
+        } else {
+		forkOS_createThread(&run_timingDelayDecrement, NULL, &err);
                 _amain();
+	}
         hs_exit();
         return 0;
 }
